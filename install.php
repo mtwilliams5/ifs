d3ff79a32906607011196a18a79e8943679500d7
Matt Williams <mwilliams28291@googlemail.com>
2017-06-04 21:51:21 +0100

    Updating generated configuration.php file to match the default provided more closely

--- install.php
+++ install.php
@@ -131,9 +131,14 @@ define(CONFIG_FILE, "configuration.php");
 * Updated By: Nolan
 *		john.pbem@gmail.com
 *
-* Version:	1.13n (Nolan Ed.)
+* Updated By: Matt Williams
+*       matt@mtwilliams.uk
+*
+* Version:	1.17
 * Release Date: June 3, 2004
 * Patch 1.13n:  December 2009
+* Patch 1.16n:  March 2014
+* Patch 1.17:   June 2017
 *
 * Copyright (C) 2003-2004 Frank Anon for Obsidian Fleet RPG
 * Distributed under the terms of the GNU General Public License
@@ -144,6 +149,8 @@ define(CONFIG_FILE, "configuration.php");
 *
 * Date:	6/03/04
 * Comments: Configuration variables
+*
+* 25/03/2014 Added variable checks to remove some basic warnings
 *
 * See CHANGELOG for patch details
 *
@@ -190,7 +197,7 @@ define(CONFIG_FILE, "configuration.php");
 \$col = 3;
 \$row = 3;
 
-if (\$directory !='uploadfiles'){
+if (isset(\$directory) && \$directory !='uploadfiles'){
 	\$title[0] = 'Story Images';
 	\$dir[0]=\$base_path . 'images/stories'; 
 	\$picurl[0]=\$live_site . '/images/stories/';
