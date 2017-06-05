d5e67a902767b3ae6ba437832340e196484daf12
Matt Williams <mwilliams28291@googlemail.com>
2017-06-05 02:41:21 +0100

    More updates to classes

--- classes/html/poll.php
+++ classes/html/poll.php
@@ -7,8 +7,12 @@
   * Developer:	Frank Anon
   * 	    	fanon@obsidianfleet.net
   *
-  * Version:	1.11
+  * Updated By: Matt Williams
+  *             matt@mtwilliams.uk
+  *
+  * Version:	1.17
   * Release Date: June 3, 2004
+  * Patch 1.17:   June 2017
   *
   * Copyright (C) 2003-2004 Frank Anon for Obsidian Fleet RPG
   * Distributed under the terms of the GNU General Public License
@@ -17,7 +21,6 @@
   * This file based on code from Mambo Site Server 4.0.12
   * Copyright (C) 2000 - 2002 Miro International Pty Ltd
   *
-  * Date: 12/13/03
   * Comments: Display poll
  ***/
 
@@ -29,126 +32,87 @@ class poll
         $months_num = array("01","02","03","04","05","06","07","08","09","10","11","12");
         ?>
 
-	    <table cellpadding="0" cellspacing="0" border="0" width="100%">
-	    <tr>
-			<td class="articlehead" colspan="2" height="40">
-            	&nbsp;&nbsp;Polls/Surveys - Results
-            </td>
-	    </tr>
-	    <tr>
-	    	<td align="right" width="181"><b>Survey Title:</b></td>
-	    	<td width="606">
-            	&nbsp;<?php echo $pollTitle ?>
-	    	</td>
-	    </tr>
-	    <tr>
-	    	<td align="right" width="181"><b>Number of voters:</b></td>
-	    	<td width="606"> &nbsp;
-	    		<?php echo $voters ?>
-	    	</td>
-	    </tr>
-	    <tr>
-	    	<td align="right" width="181"><b>First Vote:</b></td>
-	    	<td width="606"> &nbsp;
-	        	<?php echo $first_vote ?>
-	    	</td>
-	    </tr>
-	    <tr>
-			<td align="right" width="181"><b>Last Vote:</b></td>
-	    	<td width="606"> &nbsp;
-	    		<?php echo $last_vote ?>
-	    	</td>
-	    </tr>
-	    <tr>
-	    	<td colspan="3" align="center">
-	    		<table width="100%" border="0" cellspacing="0" cellpadding="0">
-	        	<tr>
-	            	<td width="33%">&nbsp;</td>
-		            <td width="26%" align="center">&nbsp;</td>
-		            <td width="41%">&nbsp;</td>
-				</tr>
-	        	<tr>
-	            	<td width="33%" height="30" align="right">Select a month:&nbsp; </td>
-	            	<td width="26%" align="left" height="30">
-	              		<select name="months" width="200" style="width:200px" onChange="document.location.href='index.php?option=surveyresult&task=Results&polls=<?php echo $pollID ?>&month=' + this.options[selectedIndex].value">
-	                	<option value="">Show All Months</option>
-	                	<?php
-                        for ($i = 0; $i < count($months); $i++)
-                        {
-                            if ($month == $months_num[$i])
-                            {
-                                ?>
-                                <option value="<?php echo $month ?>" selected="selected">
-                                <?php echo $months[$i] ?>
-                                </option>
-                                <?php
-                            }
-                            else
-                            {
-                                ?>
-                                <option value="<?php echo $months_num[$i] ?>">
-                                <?php echo $months[$i] ?>
-                                </option>
-                                <?php
-                            }
-                        }
-                        ?>
-						</select>
-	            	</td>
-		            <td width="41%" height="30">&nbsp;</td>
-				</tr>
-	          	<tr>
-	                <td width="33%">&nbsp;</td>
-	                <td width="33%">
-				    <table cellpadding="0" cellspacing="0" border="0" bgcolor="#000000" width="100%">
-	                	<?php
-                        if (count($percentInt) <> 0)
-                        {
-	                        for ($i = 0; $i < count($optionText); $i++)
-                            {
-	                            if ($percentInt[$i] <> "")
-                                {
-	                                $percentage = $count[$i]/$sum * 100;
-	                                $percentage = round($percentage, 2);
-	                                ?>
-			                    	<tr>
-			                        <td valign="top" height="40">
-                                    	<img src="images/polls/Col<?php echo $i+1 ?>M.gif" width="<?php echo $percentInt[$i] ?>" height="15" vspace="5" hspace="0" /><img src="images/polls/Col<?php echo $i+1 ?>R.gif" width="10" height="15" vspace="5" hspace="0" /><br />
-		 	                        	<?php echo "$optionText[$i] - $count[$i] ($percentage%)" ?>
-			                        </td>
-	    		                    </tr>
-	              			        <?php
-                                }
-                                else
-                                {
-                                	?>
-				                    <tr>
-			                        <td valign="top" height="40">
-                                    	<img src="images/polls/Col<?php echo $i+1 ?>M.gif" width="3" height="15" vspace="5" hspace="0" /><img src="images/polls/Col<?php echo $i+1 ?>R.gif" width="10" height="15" vspace="5" hspace="0" /><br />
-							            <?php echo "$optionText[$i] - $count[$i] (0%)" ?>
-	                        		</td>
-		 	                    	<?php
-                                }
-		                        unset($percentage);
-	                        }
-	                    }
-	                    else
-                        {
-                        	?>
-							<tr>
-	                        	<td valign="bottom">There are no results for this month.</td>
-	                    	</tr>
-	                      	<?php
-	                    }
-                        ?>
-	                </table>
-		            </td>
-		            <td width="41%">&nbsp;</td>
-	    		</tr>
-		        </table>
-		    </td>
-		</tr>
-		</table>
+	    <div id="pollresult">
+			<h3 class="articlehead">
+            	<p>&nbsp;&nbsp;Polls/Surveys - Results</p>
+	    	<ul class="list-unstyled">
+				<li><strong>Survey Title:</strong>
+            	&nbsp;<?php echo $pollTitle ?></li>
+
+	    		<li><strong>Number of voters:</strong> &nbsp;
+	    		<?php echo $voters ?></li>
+
+				<li><strong>First Vote:</strong> &nbsp;
+	        	<?php echo $first_vote ?></li>
+	    		
+				<li><strong>Last Vote:</strong> &nbsp;
+	    		<?php echo $last_vote ?></li>
+	    	</ul>
+	    </div>
+	    <div>
+			<strong>Select a month:</strong>
+			<select name="months" width="200" style="width:200px" onChange="document.location.href='index.php?option=surveyresult&amp;task=Results&amp;polls=<?php echo $pollID ?>&amp;month=' + this.options[selectedIndex].value">
+			<option value="">Show All Months</option>
+			<?php
+				for ($i = 0; $i < count($months); $i++)
+				{
+					if ($month == $months_num[$i])
+					{
+						?>
+						<option value="<?php echo $month ?>" selected="selected">
+							<?php echo $months[$i] ?>
+						</option>
+						<?php
+					}
+					else
+					{
+						?>
+						<option value="<?php echo $months_num[$i] ?>">
+							<?php echo $months[$i] ?>
+						</option>
+						<?php
+					}
+				}
+			?>
+			</select>
+		</div>
+		<div>
+			<?php
+			if (count($percentInt) <> 0)
+			{
+				for ($i = 0; $i < count($optionText); $i++)
+				{
+					if ($percentInt[$i] <> "")
+					{
+						$percentage = $count[$i]/$sum * 100;
+						$percentage = round($percentage, 2);
+						?>
+						<div>
+							<img src="images/polls/Col<?php echo $i+1 ?>M.gif" width="<?php echo $percentInt[$i] ?>" height="15" vspace="5" hspace="0" /><img src="images/polls/Col<?php echo $i+1 ?>R.gif" width="10" height="15" vspace="5" hspace="0" /><br />
+							<?php echo "$optionText[$i] - $count[$i] ($percentage%)" ?>
+						</div>
+						<?php
+					}
+					else
+					{
+						?>
+						<div>
+							<img src="images/polls/Col<?php echo $i+1 ?>M.gif" width="3" height="15" vspace="5" hspace="0" /><img src="images/polls/Col<?php echo $i+1 ?>R.gif" width="10" height="15" vspace="5" hspace="0" /><br />
+							<?php echo "$optionText[$i] - $count[$i] (0%)" ?>
+						</div>
+						<?php
+					}
+					unset($percentage);
+				}
+			}
+			else
+			{
+				?>
+				<p>There are no results for this month.</p>
+				<?php
+			}
+			?>
+		</div>
     	<?php
 	}
 }
--- classes/html/weblinks.php
+++ classes/html/weblinks.php
@@ -7,8 +7,12 @@
   * Developer:	Frank Anon
   * 	    	fanon@obsidianfleet.net
   *
-  * Version:	1.11
+  * Updated By: Matt Williams
+  *             matt@mtwilliams.uk
+  *
+  * Version:	1.17
   * Release Date: June 3, 2004
+  * Patch 1.17:   June 2017
   *
   * Copyright (C) 2003-2004 Frank Anon for Obsidian Fleet RPG
   * Distributed under the terms of the GNU General Public License
@@ -17,7 +21,6 @@
   * This file based on code from Mambo Site Server 4.0.12
   * Copyright (C) 2000 - 2002 Miro International Pty Ltd
   *
-  *	Date: 12/13/03
   *	Comments: Function displays selected weblink category and titles from the database.
  ***/
 
@@ -25,86 +28,60 @@ class weblinks {
 	function displaylist($topictext, $topicid, $title, $sid, $id, $date, $url)
     {
     	?>
-		<table width="98%" cellpadding="4" cellspacing="4" border="0" align="center">
-		<tr>
-			<td colspan="2">
-				<p class="articlehead">Web Links</p>
-			</td>
-		</tr>
-		<tr>
-	        <td width="50%" valign="top" >
-	            <p>We are regularly out on the web. When we find some great site we list
-	              them here for to enjoy, however we would love you to come back. From
-	              the box below choose one of our Link topics, then select a URL to visit.</p>
-	        </td>
-	        <td width="50%" valign="top">
-	            <!-- insert image here -->
-	        </td>
-	    </tr>
-	    <tr>
-	        <td valign="top" colspan="2">
-	            <?php
-	            if ($id <> "")
-	            {
-	                ?>
-	                <hr noshade size="1" />
-	                <table width="100%" border="0" cellspacing="0" cellpadding="0">
-	                <tr>
-	                    <td width="32" height="20" align="center" bgcolor="#999999">&nbsp;</td>
-	                    <td width="100%" height="20" bgcolor="#999999">
-	                        <b><font color="#FFFFFF">Web Link</font></b>
-	                    </td>
-	                </tr>
-	                <?php
-	                $color = array("#333333", "#666666");
-	                $k = 0;
-	                for ($i = 0; $i < count($sid["$topictext[$id]"]); $i++)
-	                {
-	                    $test = $time["$topictext[$id]"][$i];
-	                    $count = $counter["$topictext[$id]"][$i];
-	                    $date = split(" ",$test);
-	                    $datesplit = split("-", $date[0]);
-	                    ?>
+		<div>
+			<h3 class="articlehead">Web Links</h3>
+			<p>We are regularly out on the web. When we find some great site we list
+				them here for to enjoy, however we would love you to come back. From
+				the box below choose one of our Link topics, then select a URL to visit.</p>
+			<?php
+			if ($id <> "")
+			{
+				?>
+				<hr noshade size="1" />
+				<h4>Web Link</h4>
+				<?php
+				$color = array("#333333", "#666666");
+				$k = 0;
+				for ($i = 0; $i < count($sid["$topictext[$id]"]); $i++)
+				{
+					$test = $time["$topictext[$id]"][$i];
+					$count = $counter["$topictext[$id]"][$i];
+					$date = split(" ",$test);
+					$datesplit = split("-", $date[0]);
+					?>
 
-	                    <tr bgcolor = "<?php echo $color[$k] ?>">
-	                        <?php
-	                        $today = date("n d Y");
-	                        $todaydate = split(" ", $today);
-	                        $sum = $todaydate[2] - $datesplit[0];
-	                        ?>
-	                        <td width="32" height="20" align="center">
-	                            <img src="images/wwwicon.gif" width="32" height="16" align="absbottom" vspace="3" hspace="10" />
-	                        </td>
-	                        <td width="100%" height="20">
-	                            <a href="<?php echo $url["$topictext[$id]"][$i] ?>" target="_blank">
-	                            <?php echo $title["$topictext[$id]"][$i] ?></a>
-	                        </td>
-	                    </tr>
-	                    <?php
-	                    if ($k == 1)
-	                        $k = 0;
-	                    else
-	                        $k++;
-	                }
-	                ?>
-	                </table>
-	                <hr noshade size="1" />
-	                <?php
-	            }
-	            ?>
-	            <p><div class="articlehead">Categories</div><br />
-	            <?php
-	            for ($i = 0; $i < count($topicid); $i++)
-	            {
-	                if (($id == $i) && ($id <> ""))
-	                    echo "<li> <a class=\"category\">$topictext[$i]</a>&nbsp;</li>\n";
-	                else
-	                    echo "<li> <a class=\"category\" href=\"index.php?option=weblinks&Itemid=4&topid=$i\">$topictext[$i]</a>&nbsp;</li>\n";
-	            }
-	            ?>
-	        </td>
-	    </tr>
-	    </table>
+					<div style="background-color:<?php echo $color[$k] ?>">
+						<?php
+						$today = date("n d Y");
+						$todaydate = split(" ", $today);
+						$sum = $todaydate[2] - $datesplit[0];
+						?>
+						<img src="images/wwwicon.gif" width="32" height="16" align="absbottom" vspace="3" hspace="10" />
+						<a href="<?php echo $url["$topictext[$id]"][$i] ?>" target="_blank">
+						<?php echo $title["$topictext[$id]"][$i] ?></a>
+					</div>
+					<?php
+					if ($k == 1)
+						$k = 0;
+					else
+						$k++;
+				}
+				?>
+				<hr noshade size="1" />
+				<?php
+			}
+			?>
+			<h3 class="articlehead">Categories</h3>
+			<?php
+			for ($i = 0; $i < count($topicid); $i++)
+			{
+				if (($id == $i) && ($id <> ""))
+					echo "<li> <a class=\"category\">$topictext[$i]</a>&nbsp;</li>\n";
+				else
+					echo "<li> <a class=\"category\" href=\"index.php?option=weblinks&Itemid=4&topid=$i\">$topictext[$i]</a>&nbsp;</li>\n";
+			}
+			?>
+	    </div>
 		<?php
     }
 }
