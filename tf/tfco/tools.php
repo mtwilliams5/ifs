7f108790ebd255d8a0471fc33414b2f9370401ee
Matthew Williams <matt@mtwilliams.uk>
2017-07-12 23:04:42 +0100

    TFCO index and tools updates

--- tf/tfco/tools.php
+++ tf/tfco/tools.php
@@ -7,14 +7,17 @@
   * Developer:	Frank Anon
   * 	    	fanon@obsidianfleet.net
   *
-  * Version:	1.11
+  * Updated By: Matt Williams
+  *       matt@mtwilliams.uk
+  *
+  * Version:	1.17
   * Release Date: June 3, 2004
+  * Patch 1.17: June 2017
   *
   * Copyright (C) 2003-2004 Frank Anon for Obsidian Fleet RPG
   * Distributed under the terms of the GNU General Public License
   * See doc/LICENSE for details
   *
-  * Date:	1/6/04
   * Comments: TFCO Tools!
  ***/
 
@@ -23,12 +26,10 @@ if (!defined("IFS"))
 else
 {
 	?>
-	<br /><center>
-	Welcome to TFCO Tools<br />
-	Please note that your login will time out after about 10 minutes of inactivity.
-	</center><br /><br />
+	<h2 class="text-center">Welcome to TFCO Tools</h2>
+	<p class="text-center">Please note that your login will time out after about 10 minutes of inactivity.</p>
 
-	<a class="ifsheading"><font size="+1"><b>Add a Ship:</b></font></a><br />
+	<h3><strong class="ifsheading">Add a Ship:</strong></h3>
     <?php
     $qry = "SELECT c.name
             FROM {$sdb}classes c, {$sdb}category d, {$sdb}types t
@@ -38,57 +39,70 @@ else
     if ( mysql_num_rows($result) )
     {
     	?>
-	    <form action="index.php?option=ifs&task=tfco&action=common&lib=sadd" method="post">
-	        <input type="hidden" name="sid" value="na" />
-	        <input type="hidden" name="tf" value="<?php echo $tfid ?>" />
-	        <input type="hidden" name="format" value="Play By Email" />
-
-	        Ship name:
-	        <input type="text" name="sname" size="20" /><br /><br />
-
-	        Class:
-	        <select name="class">
-	            <option selectd="selected"></option>
-	            <?php
-	            while ( list ($sname) = mysql_fetch_array($result) )
-	                echo "<option value=\"$sname\">$sname</option>\n";
-	            ?>
-	        </select>
-	        <br /><br />
-
-	        Registry Number:
-	        <input type="text" name="registry" size="20" /><br /><br />
-
-	        Status:
-	        <select name="status">
-	            <?php
-	            $filename = "tf/status.txt";
-	            $contents = file($filename);
-	            $length = sizeof($contents);
-	            $count = 0;
-	            $counter = 0;
-	            do
-	            {
-	                $contents[$counter] = trim($contents[$counter]);
-	                echo "<option value=\"$contents[$counter]\">$contents[$counter]</option>\n";
-	                $counter = $counter + 1;
-	            } while ($counter < $length);
-	            ?>
-	        </select>
-	        <br /><br />
-
-	        Task Group:
-	        <select name="grpid" size="1">
-	            <?php
-	            $qry = "SELECT tg, name FROM {$spre}taskforces WHERE tf='$tfid' AND tg<>'0'";
-	            $result = $database->openConnectionWithReturn($qry);
-	            while (list($grp,$grpname)=mysql_fetch_array($result))
-	                echo "<option value=\"{$grp}\">{$grpname}</option>\n";
-	            ?>
-	        </select>
-	        <br /><br />
-
-	        <input type="submit" value="Add">
+	    <form class="form-horizontal" action="index.php?option=ifs&amp;task=tfco&amp;action=common&amp;lib=sadd" method="post">
+	        <input type="hidden" name="sid" value="na">
+	        <input type="hidden" name="tf" value="<?php echo $tfid ?>">
+	        <input type="hidden" name="format" value="Play By Nova">
+			<div class="form-group">
+	        	<label for="sname" class="col-sm-2 control-label">Ship name:</label>
+	        	<div class="col-sm-10 col-md-6 col-lg-4">
+                	<input type="text" class="form-control" name="sname" id="sname" size="20">
+                </div>
+            </div>
+			<div class="form-group">
+	        	<label for="class" class="col-sm-2 control-label">Class:</label>
+	        	<div class="col-sm-10 col-md-6 col-lg-4">
+                    <select class="form-control" name="class" id="class">
+                        <option selected="selected"></option>
+                        <?php
+                        while ( list ($sname) = mysql_fetch_array($result) )
+                            echo "<option value=\"$sname\">$sname</option>\n";
+                        ?>
+                    </select>
+                </div>
+            </div>
+			<div class="form-group">
+	        	<label for="registry" class="col-sm-2 control-label">Registry Number:</label>
+	        	<div class="col-sm-10 col-md-6 col-lg-4">
+                    <input type="text" class="form-control" name="registry" id="registry" size="20">
+                </div>
+            </div>
+			<div class="form-group">
+	        	<label for="status" class="col-sm-2 control-label">Status:</label>
+	        	<div class="col-sm-10 col-md-6 col-lg-4">
+                    <select class="form-control" name="status" id="status">
+                        <?php
+                        $filename = "tf/status.txt";
+                        $contents = file($filename);
+                        $length = sizeof($contents);
+                        $count = 0;
+                        $counter = 0;
+                        do
+                        {
+                            $contents[$counter] = trim($contents[$counter]);
+                            echo "<option value=\"$contents[$counter]\">$contents[$counter]</option>\n";
+                            $counter = $counter + 1;
+                        } while ($counter < $length);
+                        ?>
+                    </select>
+                </div>
+            </div>
+			<div class="form-group">
+	        	<label for="grpid" class="col-sm-2 control-label">Task Group:</label>
+	        	<div class="col-sm-10 col-md-6 col-lg-4">
+                    <select class="form-control" name="grpid" id="grpid">
+                        <?php
+                        $qry = "SELECT tg, name FROM {$spre}taskforces WHERE tf='$tfid' AND tg<>'0'";
+                        $result = $database->openConnectionWithReturn($qry);
+                        while (list($grp,$grpname)=mysql_fetch_array($result))
+                            echo "<option value=\"{$grp}\">{$grpname}</option>\n";
+                        ?>
+                    </select>
+                </div>
+            </div>
+			<div class="form-group">
+		        <input class="btn btn-default" type="submit" value="Add">
+            </div>
 	    </form>
         <?php
     }
@@ -97,10 +111,10 @@ else
     ?>
 	<br /><br />
 
-	<a class="heading"><font size="+1"><b>Add a CO:</b></font></a><br />
-	<i>Only use this to add a CO that is <b>not</b> already in IFS!
-    If they are already in the system (ie, they submitted an app through OFHQ),
-    assign the CO through ship admin!</i><br />
+	<h3><strong class="heading">Add a CO:</strong></h3>
+	<span class="help-block">Only use this to add a CO that is <strong>not</strong> already in IFS!
+    If they are already in the system (ie, they submitted an app through IFS),
+    assign the CO through sim admin!</span>
 
 	<?php
     $qry = "SELECT id, name FROM {$spre}ships WHERE tf='$tfid' AND co='0'";
@@ -108,16 +122,22 @@ else
 
     if (mysql_num_rows($result))
     {
-		echo "<form action=\"index.php?option=ifs&amp;task=tfco&amp;action=common&amp;lib=cadd\" method=\"post\">\n";
-		echo "<select name=\"sid\">\n";
-	    while (list ($sid, $sname) = mysql_fetch_array($result))
-	    	echo "<option value=\"$sid\">$sname</option>\n";
-		echo "</select>\n";
-		echo "<input type=\"submit\" value=\"Submit\" /></form>\n";
+	?>
+		<form class="form-inline" action="index.php?option=ifs&amp;task=tfco&amp;action=common&amp;lib=cadd" method="post">
+            <div class="form-group">
+                <label for="sid" class="sr-only">Ship to add a CO to:</label>
+                <select class="form-control" name="sid" id="sid">
+                <?php 
+                while (list ($sid, $sname) = mysql_fetch_array($result))
+                    echo '<option value="' . $sid . '">' . $sname . '</option>';
+                ?>
+                </select>
+            </div>
+            <input class="btn btn-default btn-sm" type="submit" value="Submit">
+        </form>
+    <?php
     }
     else
-    	echo "<br />(not applicable - all your ships have COs!  Congrats!)\n";
-
-	echo "<br /><br />\n";
+    	echo '<h5>(not applicable - all your sims have COs!  Congrats!)</h5>';
 }
 ?>
